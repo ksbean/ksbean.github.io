6e7e8f61dc86ea8be95e8485cab3ae8687a3813a
Kenny Bean <kenny.bean.external@anbs.com>
2017-05-04 09:00:07 -0400

    final site added to directory

---
+++ IndexJS.js
@@ -0,0 +1,15 @@
+﻿
+function Daboutme() {
+    var y = document.getElementById('Aboutme');
+    var x = document.getElementById('wrapper');
+        y.style.display = 'inline';
+        x.style.display = 'none';
+}
+function DHome(){
+    var x = document.getElementById('Aboutme');
+    var y = document.getElementById('wrapper');
+    y.style.display = 'inline';
+    x.style.display = 'none';
+}
+
+
